9bd5995f605dc8e0826550ed2107522bcbad9f51
lsky88 <from.russia@hotmail.com>
2022-02-04 00:12:26 +0500

    16. Props

--- src/components/ProfilePage/Posts/MyPosts/MyPosts.jsx
+++ src/components/ProfilePage/Posts/MyPosts/MyPosts.jsx
@@ -7,11 +7,9 @@ const MyPosts = () => {
     return (
         <section className={style.myPosts}>
             <h2>Posts</h2>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post  user="James" message="Hello" id="1" likesCount="3"/>
+            <Post user="Sandra" message="What's up?" id="2" likesCount="5"/>
+            <Post user="Andy" message="Yo" id="3" likesCount="7"/>
         </section>
     )
 }
--- src/components/ProfilePage/Posts/MyPosts/Post/Post.jsx
+++ src/components/ProfilePage/Posts/MyPosts/Post/Post.jsx
@@ -2,15 +2,15 @@
 import style from "./Post.module.css";
 
 // Post ===========================
-const Post = () => {
-  return (
-      <div className={style.postContainer}>
-          <div className={style.user}></div>
-          James:
-          <div className={style.post}>Hello <span>❤︎ 5</span></div>
-
-      </div>
-  )
+const Post = (props) => {
+    return (
+        <div className={style.postContainer}>
+            <div className={style.user}></div>
+            {props.user}:
+            <div className={style.post} id={props.id}><p>{props.message}</p>
+                <span>❤︎ {props.likesCount}</span></div>
+        </div>
+    )
 }
 
 export default Post;
